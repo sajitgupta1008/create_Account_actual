5febc7be6a3e1546438e83691202731f84b700d3
Nikko Mina <nmina@rccl.com>
2017-07-06 17:03:05 -0400

    Updated invalid password exception message

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/exceptions/InvalidGuestException.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/exceptions/InvalidGuestException.java
@@ -16,7 +16,7 @@ public class InvalidGuestException extends MiddlewareTransportException {
     static {
         Map<String, String> passwordError = new HashMap<>();
         passwordError.put("password", "The password must be between 7 and 32 characters, inclusive, " +
-                "with at least seven (6) letters and one (1) special character or a number.");
+                "with at least one (1) special character or a number.");
         INVALID_PASSWORD = new InvalidGuestException(passwordError);
     }
     
