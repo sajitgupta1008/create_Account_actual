b753f8632ff31f64a4b467770c01529ce076df04
Nikko Mina <nmina@rccl.com>
2017-07-06 22:23:01 -0400

    Updated validation rules for terms and conditions

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/TermsAndConditionsAgreement.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/TermsAndConditionsAgreement.java
@@ -4,17 +4,19 @@ import com.lightbend.lagom.serialization.Jsonable;
 import com.rccl.middleware.common.validation.validator.DateFormat;
 import lombok.Builder;
 import lombok.Value;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
 
 @Value
 @Builder
 public class TermsAndConditionsAgreement implements Jsonable {
-    
-    @NotNull(message = "The acceptance time in ISO-8601 date format (yyyyMMddHHmmssaa) is required.", groups = Guest.DefaultChecks.class)
+    @NotBlank(message = "The acceptance time in ISO-8601 date format (yyyyMMddHHmmssaa) is required.", groups = Guest.CreateChecks.class)
+    @NotNull(message = "The acceptance time in ISO-8601 date format (yyyyMMddHHmmssaa) is required.", groups = Guest.UpdateChecks.class)
     @DateFormat(format = "yyyyMMddHHmmssaa", message = "The date must follow ISO-8601 format(yyyyMMddHHmmssaa).", groups = Guest.DefaultChecks.class)
     String acceptTime;
     
-    @NotNull(message = "The terms and conditions version number is required.", groups = Guest.DefaultChecks.class)
+    @NotBlank(message = "The terms and conditions version number is required.", groups = Guest.CreateChecks.class)
+    @NotNull(message = "The terms and conditions version number is required.", groups = Guest.UpdateChecks.class)
     String version;
 }
