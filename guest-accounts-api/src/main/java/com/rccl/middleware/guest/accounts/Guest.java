b96763df037478674dd1bdbc8f9fc2d448443c1d
Nikko Mina <nmina@rccl.com>
2017-07-11 14:04:25 -0400

    Added header validation.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
@@ -24,6 +24,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Guest implements Jsonable {
     
+    @NotNull(message = "A header is required.", groups = DefaultChecks.class)
     @Valid
     Header header;
     
