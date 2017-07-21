f9516fe80f8a147226a05b6055b3ea6da7bff088
Nikko Mina <nmina@rccl.com>
2017-07-21 12:22:33 -0400

    Updated validation to refer on optins maven dependency.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Optin.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Optin.java
@@ -1,6 +1,7 @@
 package com.rccl.middleware.guest.accounts;
 
 import com.rccl.middleware.common.validation.validator.DateFormat;
+import com.rccl.middleware.guest.optin.validation.ValidOptin;
 import lombok.Builder;
 import lombok.Value;
 
@@ -10,11 +11,12 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class Optin {
     
+    @ValidOptin(groups = Guest.DefaultChecks.class)
     String type;
     
     boolean flag;
     
-    @NotNull(message = "The acceptance time in ISO-8601 date format (yyyyMMddHHmmssaa) is required.", groups = Guest.DefaultChecks.class)
-    @DateFormat(format = "yyyyMMddHHmmssaa", message = "The date must follow ISO-8601 format(yyyyMMddHHmmssaa).", groups = Guest.DefaultChecks.class)
+    @NotNull(message = "The acceptance time in ISO-8601 date format (yyyyMMddhhmmssaa) is required.", groups = Guest.DefaultChecks.class)
+    @DateFormat(format = "yyyyMMddhhmmssaa", message = "The date must follow ISO-8601 format(yyyyMMddhhmmssaa).", groups = Guest.DefaultChecks.class)
     String acceptTime;
 }
