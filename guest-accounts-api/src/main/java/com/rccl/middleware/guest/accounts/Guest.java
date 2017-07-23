87b8287fc7641fdc7570adb2d2381157aee6fda3
Nikko Mina <nmina@rccl.com>
2017-07-22 22:10:55 -0400

    Made phone number optional.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
@@ -55,7 +55,6 @@ public class Guest implements Jsonable {
     @DateOfBirth(groups = DefaultChecks.class)
     String birthdate;
     
-    @NotEmpty(message = "A phone number is required.", groups = CreateChecks.class)
     @Size(min = 7, max = 30, message = "The phone number must be at least seven (7) characters"
             + " and maximum of thirty (30) characters.", groups = DefaultChecks.class)
     @Pattern(regexp = "[0-9+()-]*", message = "The phone number is invalidly formatted", groups = DefaultChecks.class)
