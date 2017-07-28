4535084fa2c689c5060602ed23995e19461cf29e
Nikko Mina <nmina@rccl.com>
2017-07-28 17:21:52 -0400

    Updated Guest max length to 256.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/Guest.java
@@ -29,7 +29,7 @@ public class Guest implements Jsonable {
     Header header;
     
     @NotBlank(message = "An email is required.", groups = DefaultChecks.class)
-    @Size(min = 5, max = 100, message = "The email can only have up to 100 characters.", groups = DefaultChecks.class)
+    @Size(min = 5, max = 256, message = "The email can only have up to 256 characters.", groups = DefaultChecks.class)
     @Email(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
             + "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])"
             + "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]"
