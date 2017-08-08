e1e7eae4ccc181c1810e1dbbf4241669ff7dc313
Nikko Mina <nmina@rccl.com>
2017-08-08 18:30:44 -0400

    Added enriched update service endpoint.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/GuestAccountService.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/GuestAccountService.java
@@ -6,6 +6,7 @@ import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.api.broker.Topic;
+import com.lightbend.lagom.javadsl.server.HeaderServiceCall;
 import com.typesafe.config.ConfigFactory;
 
 import static com.lightbend.lagom.javadsl.api.Service.named;
@@ -21,15 +22,11 @@ public interface GuestAccountService extends Service {
     
     String LINK_LOYALTY_KAFKA_TOPIC = ConfigFactory.load().getString("kafka.link-loyalty.topic.name");
     
-    /**
-     * Create a guest account from the given {@link Guest} and get back the user ID
-     * representing the created resource.
-     *
-     * @return {@code String}
-     */
-    ServiceCall<Guest, JsonNode> createAccount();
+    HeaderServiceCall<Guest, JsonNode> createAccount();
     
     ServiceCall<Guest, NotUsed> updateAccount(String email);
+    
+    ServiceCall<JsonNode, NotUsed> updateAccountEnriched();
     
     ServiceCall<AccountCredentials, JsonNode> authenticateUser();
     
@@ -48,6 +45,7 @@ public interface GuestAccountService extends Service {
                         restCall(POST, "/v1/guestAccounts", this::createAccount),
                         restCall(POST, "/v1/guestAccounts/", this::createAccount),
                         restCall(PUT, "/v1/guestAccounts/:email", this::updateAccount),
+                        restCall(PUT, "/v1/guestAccounts/enriched", this::updateAccountEnriched),
                         restCall(POST, "/v1/guestAccounts/login", this::authenticateUser),
                         restCall(GET, "/v1/guestAccounts/:email/validation", this::validateEmail),
                         restCall(GET, "/v1/guestAccounts/health", this::healthCheck)
