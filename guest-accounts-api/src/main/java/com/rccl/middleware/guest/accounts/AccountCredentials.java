ff817cd96633f391630c2b3fb1232ec6383bd3dc
Nikko Mina <nmina@rccl.com>
2017-08-23 12:13:47 -0400

    Removed constraints in favor of webshopper login credentials.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/AccountCredentials.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/AccountCredentials.java
@@ -3,11 +3,8 @@ package com.rccl.middleware.guest.accounts;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.lightbend.lagom.serialization.Jsonable;
 import com.rccl.middleware.common.header.Header;
-import com.rccl.middleware.common.validation.validator.GuestAccountPassword;
-import com.rccl.middleware.common.validation.validator.ValidatorConstants;
 import lombok.Builder;
 import lombok.Value;
-import org.hibernate.validator.constraints.Email;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -22,10 +19,8 @@ public class AccountCredentials implements Jsonable {
     Header header;
     
     @NotNull(message = "A username is required.")
-    @Email(regexp = ValidatorConstants.EMAIL_REGEXP)
     String username;
     
     @NotNull(message = "A password is required.")
-    @GuestAccountPassword
     char[] password;
 }
