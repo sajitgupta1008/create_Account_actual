6048762969313cb811f6ef62181bd96864a7937f
Nikko Mina <nmina@rccl.com>
2017-08-27 23:24:48 -0400

    Added more attributes for GET enrichedGuest.

--- guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/enriched/LoyaltyInformation.java
+++ guest-accounts-api/src/main/java/com/rccl/middleware/guest/accounts/enriched/LoyaltyInformation.java
@@ -1,6 +1,10 @@
 package com.rccl.middleware.guest.accounts.enriched;
 
 import com.lightbend.lagom.serialization.Jsonable;
+import com.rccl.middleware.guestprofiles.models.CaptainsClubLoyaltyTier;
+import com.rccl.middleware.guestprofiles.models.CelebrityBlueChipLoyaltyTier;
+import com.rccl.middleware.guestprofiles.models.ClubRoyaleLoyaltyTier;
+import com.rccl.middleware.guestprofiles.models.CrownAndAnchorSocietyLoyaltyTier;
 import lombok.Builder;
 import lombok.Value;
 
@@ -10,14 +14,23 @@ import javax.validation.constraints.Pattern;
 @Value
 public class LoyaltyInformation implements Jsonable {
     
-    @Pattern(regexp = "\\d*", message = "Consumer ID must be in numeric format.")
-    String consumerId;
-    
     @Pattern(regexp = "\\d*", message = "Crown and Anchor Loyalty ID must be in numeric format.")
     String crownAndAnchorId;
     
+    CrownAndAnchorSocietyLoyaltyTier crownAndAnchorSocietyLoyaltyTier;
+    
+    Integer crownAndAnchorSocietyLoyaltyIndividualPoints;
+    
+    Integer crownAndAnchorSocietyLoyaltyRelationshipPoints;
+    
     @Pattern(regexp = "\\d*", message = "Captains Club Loyalty ID must be in numeric format.")
     String captainsClubId;
+    
+    CaptainsClubLoyaltyTier captainsClubLoyaltyTier;
+    
+    Integer captainsClubLoyaltyIndividualPoints;
+    
+    Integer captainsClubLoyaltyRelationshipPoints;
     
     @Pattern(regexp = "\\d*", message = "Azamara Loyalty ID must be in numeric format.")
     String azamaraLoyaltyId;
@@ -25,6 +38,18 @@ public class LoyaltyInformation implements Jsonable {
     @Pattern(regexp = "\\d*", message = "Club Royale Loyalty ID must be in numeric format.")
     String clubRoyaleId;
     
+    ClubRoyaleLoyaltyTier clubRoyaleLoyaltyTier;
+    
+    Integer clubRoyaleLoyaltyIndividualPoints;
+    
+    Integer clubRoyaleLoyaltyRelationshipPoints;
+    
     @Pattern(regexp = "\\d*", message = "Celebrity Blue Chip Loyalty ID must be in numeric format.")
     String celebrityBlueChipId;
+    
+    CelebrityBlueChipLoyaltyTier celebrityBlueChipLoyaltyTier;
+    
+    Integer celebrityBlueChipLoyaltyIndividualPoints;
+    
+    Integer celebrityBlueChipLoyaltyRelationshipPoints;
 }
