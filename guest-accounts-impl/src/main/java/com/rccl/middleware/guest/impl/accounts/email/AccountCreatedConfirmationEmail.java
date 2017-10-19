515a27a6b7ced3fd5f954674a3beaf01aae4b384
Roberto Aleman Jr <raleman@rccl.com>
2017-10-19 14:15:45 -0400

    Updated logic to leverage the new AemEmailService.

--- guest-accounts-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/email/AccountCreatedConfirmationEmail.java
+++ guest-accounts-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/email/AccountCreatedConfirmationEmail.java
@@ -1,39 +1,44 @@
 package com.rccl.middleware.guest.impl.accounts.email;
 
 import ch.qos.logback.classic.Logger;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.lightbend.lagom.javadsl.api.transport.TransportErrorCode;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
-import com.rccl.middleware.aem.api.AemService;
+import com.rccl.middleware.aem.api.email.AemEmailService;
+import com.rccl.middleware.aem.api.models.HtmlEmailTemplate;
 import com.rccl.middleware.common.exceptions.MiddlewareTransportException;
 import com.rccl.middleware.common.logging.RcclLoggerFactory;
-import com.rccl.middleware.guest.accounts.email.EmailNotification;
 import com.rccl.middleware.guest.accounts.Guest;
+import com.rccl.middleware.guest.accounts.email.EmailNotification;
 
 import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 public class AccountCreatedConfirmationEmail {
     
     private static final Logger LOGGER = RcclLoggerFactory.getLogger(AccountCreatedConfirmationEmail.class);
     
-    private AemService aemService;
+    private AemEmailService aemEmailService;
     
     private PersistentEntityRegistry persistentEntityRegistry;
     
     @Inject
-    public AccountCreatedConfirmationEmail(AemService aemService,
+    public AccountCreatedConfirmationEmail(AemEmailService aemEmailService,
                                            PersistentEntityRegistry persistentEntityRegistry) {
-        this.aemService = aemService;
+        this.aemEmailService = aemEmailService;
         this.persistentEntityRegistry = persistentEntityRegistry;
     }
     
     public void send(Guest guest) {
-        this.getAccountCreatedConfirmationEmailTemplate()
-                .thenAccept(aemTemplateResponse -> {
-                    String content = this.getPopulatedEmailTemplate(aemTemplateResponse);
+        if (guest == null) {
+            throw new IllegalArgumentException("The Guest argument is required.");
+        }
+        
+        this.getEmailContent(guest)
+                .thenAccept(htmlEmailTemplate -> {
+                    String content = htmlEmailTemplate.getHtmlMessage();
                     String sender = "notifications@rccl.com";
-                    String subject = aemTemplateResponse.get("subject").asText();
+                    String subject = htmlEmailTemplate.getSubject();
                     
                     EmailNotification en = EmailNotification.builder()
                             .recipient(guest.getEmail())
@@ -46,20 +51,34 @@ public class AccountCreatedConfirmationEmail {
                 });
     }
     
-    private CompletionStage<JsonNode> getAccountCreatedConfirmationEmailTemplate() {
-        // TODO: Add new call for new template.
-        // TODO: aemService.getAccountCreatedConfirmationEmailTemplate()
-        return aemService.getResetPasswordEmailMigration()
-                .invoke()
-                .exceptionally(throwable -> {
-                    LOGGER.error("#getAccountCreatedConfirmationEmailTemplate:", throwable);
-                    throw new MiddlewareTransportException(TransportErrorCode.fromHttp(500), throwable);
-                });
-    }
-    
-    private String getPopulatedEmailTemplate(JsonNode aemTemplateResponse) {
-        // TODO: Add logic for populating.
-        return aemTemplateResponse.get("htmlMessage").asText();
+    private CompletionStage<HtmlEmailTemplate> getEmailContent(Guest guest) {
+        if (guest.getHeader() == null) {
+            throw new IllegalArgumentException("The header property in the Guest must not be null.");
+        }
+        
+        Character brand = guest.getHeader().getBrand();
+        
+        if (brand == null) {
+            throw new IllegalArgumentException("The brand header property in the Guest must not be null.");
+        }
+        
+        String firstName = guest.getFirstName();
+        Function<Throwable, ? extends HtmlEmailTemplate> exceptionally = throwable -> {
+            LOGGER.error("#getEmailContent:", throwable);
+            throw new MiddlewareTransportException(TransportErrorCode.fromHttp(500), throwable);
+        };
+        
+        if ('C' == brand || 'c' == brand) {
+            return aemEmailService.getCelebrityAccountCreatedConfirmationEmailContent(firstName)
+                    .invoke()
+                    .exceptionally(exceptionally);
+        } else if ('R' == brand || 'r' == brand) {
+            return aemEmailService.getRoyalAccountCreatedConfirmationEmailContent(firstName)
+                    .invoke()
+                    .exceptionally(exceptionally);
+        }
+        
+        throw new IllegalArgumentException("An invalid brand value was encountered: " + brand);
     }
     
     private void sendToTopic(EmailNotification emailNotification) {
--- guest-accounts-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/email/EmailUpdatedConfirmationEmail.java
+++ guest-accounts-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/email/EmailUpdatedConfirmationEmail.java
@@ -1,40 +1,44 @@
 package com.rccl.middleware.guest.impl.accounts.email;
 
 import ch.qos.logback.classic.Logger;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.lightbend.lagom.javadsl.api.transport.TransportErrorCode;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
-import com.rccl.middleware.aem.api.AemService;
+import com.rccl.middleware.aem.api.email.AemEmailService;
+import com.rccl.middleware.aem.api.models.HtmlEmailTemplate;
 import com.rccl.middleware.common.exceptions.MiddlewareTransportException;
 import com.rccl.middleware.common.logging.RcclLoggerFactory;
-import com.rccl.middleware.guest.accounts.Guest;
 import com.rccl.middleware.guest.accounts.email.EmailNotification;
 import com.rccl.middleware.guest.accounts.enriched.EnrichedGuest;
 
 import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 public class EmailUpdatedConfirmationEmail {
     
     private static final Logger LOGGER = RcclLoggerFactory.getLogger(EmailUpdatedConfirmationEmail.class);
     
-    private AemService aemService;
+    private AemEmailService aemEmailService;
     
     private PersistentEntityRegistry persistentEntityRegistry;
     
     @Inject
-    public EmailUpdatedConfirmationEmail(AemService aemService,
-                                           PersistentEntityRegistry persistentEntityRegistry) {
-        this.aemService = aemService;
+    public EmailUpdatedConfirmationEmail(AemEmailService aemEmailService,
+                                         PersistentEntityRegistry persistentEntityRegistry) {
+        this.aemEmailService = aemEmailService;
         this.persistentEntityRegistry = persistentEntityRegistry;
     }
     
     public void send(EnrichedGuest eg) {
-        this.getEmailUpdatedConfirmationEmailTemplate()
-                .thenAccept(aemTemplateResponse -> {
-                    String content = this.getPopulatedEmailTemplate(aemTemplateResponse);
+        if (eg == null) {
+            throw new IllegalArgumentException("The EnrichedGuest argument is required.");
+        }
+        
+        this.getEmailContent(eg)
+                .thenAccept(htmlEmailTemplate -> {
+                    String content = htmlEmailTemplate.getHtmlMessage();
                     String sender = "notifications@rccl.com";
-                    String subject = aemTemplateResponse.get("subject").asText();
+                    String subject = htmlEmailTemplate.getSubject();
                     
                     EmailNotification en = EmailNotification.builder()
                             .recipient(eg.getEmail())
@@ -47,20 +51,34 @@ public class EmailUpdatedConfirmationEmail {
                 });
     }
     
-    private CompletionStage<JsonNode> getEmailUpdatedConfirmationEmailTemplate() {
-        // TODO: Add new call for new template.
-        // TODO: aemService.getEmailUpdatedConfirmationEmailTemplate()
-        return aemService.getResetPasswordEmailMigration()
-                .invoke()
-                .exceptionally(throwable -> {
-                    LOGGER.error("#getEmailUpdatedConfirmationEmailTemplate:", throwable);
-                    throw new MiddlewareTransportException(TransportErrorCode.fromHttp(500), throwable);
-                });
-    }
-    
-    private String getPopulatedEmailTemplate(JsonNode aemTemplateResponse) {
-        // TODO: Add logic for populating.
-        return aemTemplateResponse.get("htmlMessage").asText();
+    private CompletionStage<HtmlEmailTemplate> getEmailContent(EnrichedGuest eg) {
+        if (eg.getHeader() == null) {
+            throw new IllegalArgumentException("The header property in the EnrichedGuest must not be null.");
+        }
+        
+        Character brand = eg.getHeader().getBrand();
+        
+        if (brand == null) {
+            throw new IllegalArgumentException("The brand header property in the EnrichedGuest must not be null.");
+        }
+        
+        String firstName = eg.getPersonalInformation().getFirstName();
+        Function<Throwable, ? extends HtmlEmailTemplate> exceptionally = throwable -> {
+            LOGGER.error("#getEmailContent:", throwable);
+            throw new MiddlewareTransportException(TransportErrorCode.fromHttp(500), throwable);
+        };
+        
+        if ('C' == brand || 'c' == brand) {
+            return aemEmailService.getCelebrityEmailUpdatedConfirmationEmailContent(firstName)
+                    .invoke()
+                    .exceptionally(exceptionally);
+        } else if ('R' == brand || 'r' == brand) {
+            return aemEmailService.getRoyalEmailUpdatedConfirmationEmailContent(firstName)
+                    .invoke()
+                    .exceptionally(exceptionally);
+        }
+        
+        throw new IllegalArgumentException("An invalid brand value was encountered: " + brand);
     }
     
     private void sendToTopic(EmailNotification emailNotification) {
