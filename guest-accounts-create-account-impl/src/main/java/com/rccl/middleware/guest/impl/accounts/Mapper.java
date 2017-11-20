e371a541ab4c90009037c1dc28a2ee51741ce447
Roberto Aleman Jr <raleman@rccl.com>
2017-11-20 18:35:38 -0500

    Updated the token split to ensure it only happens on the first instance of T.

--- guest-accounts-create-account-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/Mapper.java
+++ guest-accounts-create-account-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/Mapper.java
@@ -122,7 +122,7 @@ public class Mapper {
             builder.termsAndConditionsVersion(tac.getVersion());
             
             if (StringUtils.isNotBlank(tac.getAcceptTime())) {
-                String[] acceptTimeTokens = tac.getAcceptTime().split("T");
+                String[] acceptTimeTokens = tac.getAcceptTime().split("T", 1);
                 
                 if (ArrayUtils.isNotEmpty(acceptTimeTokens)) {
                     builder.termsAndConditionsAcceptDate(acceptTimeTokens[0]);
@@ -139,7 +139,7 @@ public class Mapper {
             builder.privacyPolicyVersion(ppa.getVersion());
             
             if (StringUtils.isNotBlank(ppa.getAcceptTime())) {
-                String[] acceptTimeTokens = ppa.getAcceptTime().split("T");
+                String[] acceptTimeTokens = ppa.getAcceptTime().split("T", 1);
                 
                 if (ArrayUtils.isNotEmpty(acceptTimeTokens)) {
                     builder.privacyPolicyAcceptDate(acceptTimeTokens[0]);
