0ecb00f1bbc69d9ff21113a33ec0dd812406d1a1
Nikko Mina <nmina@rccl.com>
2017-12-15 15:00:32 -0500

    Added logging for persistence entity.

--- guest-accounts-create-account-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/GuestAccountEntity.java
+++ guest-accounts-create-account-impl/src/main/java/com/rccl/middleware/guest/impl/accounts/GuestAccountEntity.java
@@ -1,7 +1,9 @@
 package com.rccl.middleware.guest.impl.accounts;
 
 import akka.Done;
+import ch.qos.logback.classic.Logger;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntity;
+import com.rccl.middleware.common.logging.RcclLoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -10,19 +12,27 @@ import java.util.Optional;
 
 public class GuestAccountEntity extends PersistentEntity<GuestAccountCommand, GuestAccountEvent, GuestAccountState> {
     
+    private static final Logger LOGGER = RcclLoggerFactory.getLogger(GuestAccountEntity.class);
+    
     @Override
     @SuppressWarnings("unchecked")
     public Behavior initialBehavior(Optional<GuestAccountState> snapshotState) {
         BehaviorBuilder builder = newBehaviorBuilder(snapshotState.orElse(GuestAccountState.emptyState()));
         
-        builder.setCommandHandler(GuestAccountCommand.CreateGuest.class, (cmd, ctx) ->
-                ctx.thenPersist(new GuestAccountEvent.GuestCreated(cmd.getGuest()),
-                        evt -> ctx.reply(Done.getInstance()))
+        builder.setCommandHandler(GuestAccountCommand.CreateGuest.class,
+                (cmd, ctx) -> {
+                    LOGGER.debug("Adding GuestAccountEvent.GuestCreated event handler.");
+                    return ctx.thenPersist(new GuestAccountEvent.GuestCreated(cmd.getGuest()),
+                            evt -> ctx.reply(Done.getInstance()));
+                }
         );
         
         builder.setEventHandler(GuestAccountEvent.GuestCreated.class,
-                evt -> new GuestAccountState(evt.getGuest(), null,
-                        LocalDateTime.now().toString(), GuestEventStatus.CREATE));
+                evt -> {
+                    LOGGER.debug("Executing GuestAccountEvent.GuestCreated handler.");
+                    return new GuestAccountState(evt.getGuest(), null,
+                            LocalDateTime.now().toString(), GuestEventStatus.CREATE);
+                });
         
         builder.setCommandHandler(GuestAccountCommand.UpdateGuest.class, (cmd, ctx) -> {
             if (cmd.getEnrichedGuest() == null) {
@@ -31,12 +41,14 @@ public class GuestAccountEntity extends PersistentEntity<GuestAccountCommand, Gu
             }
             
             List<GuestAccountEvent> events = new ArrayList<>();
+            LOGGER.debug("Adding GuestAccountEvent.GuestUpdated event handler.");
             events.add(new GuestAccountEvent.GuestUpdated(cmd.getEnrichedGuest()));
             
             // add a verify loyalty event for Kafka publishing 
             // if loyalty information is not null.
             if (cmd.getEnrichedGuest().getLoyaltyInformation() != null
                     || (cmd.getEnrichedGuest().getPersonalInformation() != null)) {
+                LOGGER.debug("Adding GuestAccountEvent.VerifyLoyalty event handler.");
                 events.add(new GuestAccountEvent.VerifyLoyalty(cmd.getEnrichedGuest()));
             }
             
@@ -44,8 +56,17 @@ public class GuestAccountEntity extends PersistentEntity<GuestAccountCommand, Gu
         });
         
         builder.setEventHandler(GuestAccountEvent.GuestUpdated.class,
-                evt -> new GuestAccountState(null, evt.getEnrichedGuest(), LocalDateTime.now().toString(),
-                        GuestEventStatus.UPDATE));
+                evt -> {
+                    LOGGER.debug("Executing GuestAccountEvent.GuestUpdated event handler.");
+                    return new GuestAccountState(null, evt.getEnrichedGuest(), LocalDateTime.now().toString(),
+                            GuestEventStatus.UPDATE);
+                });
+        
+        builder.setEventHandler(GuestAccountEvent.VerifyLoyalty.class, evt -> {
+            LOGGER.debug("Executing GuestAccountEvent.VerifyLoyalty event handler.");
+            return new GuestAccountState(null, evt.getEnrichedGuest(), LocalDateTime.now().toString(),
+                    GuestEventStatus.UPDATE);
+        });
         
         return builder.build();
     }
