1d1964d63313a513bc5cc9a482491bce39a45157
Roberto Aleman Jr <raleman@rccl.com>
2017-12-22 16:17:52 -0500

    Whitespace.

--- guest-accounts-create-account-impl/src/test/java/com/rccl/middleware/guest/impl/accounts/GuestAccountServiceTest.java
+++ guest-accounts-create-account-impl/src/test/java/com/rccl/middleware/guest/impl/accounts/GuestAccountServiceTest.java
@@ -237,7 +237,7 @@ public class GuestAccountServiceTest {
                 .build();
         
         builder.termsAndConditionsAgreement(tca);
-    
+        
         PrivacyPolicyAgreement ppa = PrivacyPolicyAgreement.builder()
                 .acceptTime("20170627T033735UTC")
                 .version("1.0")
